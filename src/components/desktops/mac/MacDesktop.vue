9d2d4946906345914c9f1ef8a3f5ad3cce603265
brendacs <brenda.zhang2@gmail.com>
2020-03-21 13:20:45 -0700

    Component structure and initial configs

---
+++ src/components/desktops/mac/MacDesktop.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="desktop">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MacDesktop'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../../../assets/scss/mixins";
+
+.desktop {
+  width: 100vw;
+  height: 100vh;
+  background: url('../../../assets/images/lakeside_wallpaper.jpg');
+  background-size: cover;
+  padding: 30px 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+</style>
